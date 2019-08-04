4b7381772f12b6599fbfe99451b8f9f734ac4eb9
kunalbidkar <kunalbidkar13@gmail.com>
2019-08-04 14:55:21 -0700

    added hellp func

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 
-def __name__ == "__main__":
+def print_hello():
     print("Hi world!")
     # print("Hi world, again!")
     print("Hi statement added by Kunal")
 
+
+
+def __name__ == "__main__":
+    print_hello()
